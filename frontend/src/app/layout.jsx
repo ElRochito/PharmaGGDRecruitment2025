0094e66560aa3fb827bc9f9cf3b055023054d3d8
Julien Roche <julien.roche@soyhuce.fr>
2025-09-25 19:06:57 +0200

    fix(sessin): ajout provider

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,18 +1,18 @@
 import './globals.css'
-import { SessionProvider } from 'next-auth/react';
 import { adminAuth } from "App/lib/auth-admin"
 import { clientAuth } from "App/lib/auth-client"
+import Providers from './Providers';
 export default async function RootLayout({ children }) {
   const sessionAdmin = await adminAuth()
   const sessionClient = await clientAuth()
   return (
     <html className="h-full bg-gray-900" lang="fr">
       <body className="h-full">
-        <SessionProvider session={sessionAdmin || sessionClient}>
+        <Providers session={sessionAdmin || sessionClient}>
           <div className="max-w-screen overflow-x-hidden">
             {children}
           </div>
-        </SessionProvider>
+        </Providers>
       </body>
     </html>
   )
